4e5bb17921403830384135956a1cee681ed695ec
m <mayashankar227@gmail.com>
2021-08-03 11:08:43 -0400

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+
+
+const image = document.querySelector("img");
+
+image.addEventListener("mouseover", (e) =>{
+  image.style.height ="300px";
+  image.style.width ="300px";
+
+});
+
+image.addEventListener("mouseleave", (e) =>{
+  image.style.height ="200px";
+  image.style.width ="200px";
+
+});
+
+image.addEventListener("click", (e) =>)
